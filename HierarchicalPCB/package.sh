4c9ef4535c4fb2bb3d09f2cc47f5dac4a41888e6
Gaurav Manek <gaurav@gauravmanek.com>
2023-09-10 14:45:15 +0800

    Icon autogenerates

--- HierarchicalPCB/package.sh
+++ HierarchicalPCB/package.sh
@@ -2,11 +2,40 @@
 
 set -e
 setopt CSH_NULL_GLOB
-
-
 echo "Generates a package for the KiCAD Repository"
 
-rm -r ./**/__pycache__ || true
+echo "Fetching repository."
+if [[ ! -d plugins_src ]]; then
+    git clone --single-branch git@github.com:gauravmm/HierarchicalPcb.git plugins_src
+fi
+(   cd plugins_src; 
+    git reset --hard;
+    git clean -fdx;
+    git pull origin master;
+    git fetch --tags;
+)
+# Get the current tag, if one exists.
+CURRTAG="$(cd plugins_src; git describe --tags)"
+
+# Generate icon if not exists
+if [[ ! -f resources/icon.png ]]; then
+    echo "Generating icon."
+    convert plugins_src/images/icon-orig.png -resize 24x24 resources/icon.png 
+fi
+
+echo "Copying files to package dir."
+if [[ -d plugins ]]; then
+    rm -rf plugins
+fi
+mkdir plugins
+rsync -av \
+    --exclude .git \
+    --exclude .gitignore \
+    --exclude README.md \
+    --exclude images \
+    --exclude '**/*.fbp' \
+    --exclude '**/__pycache__' \
+    plugins_src/ plugins
 
 UNPACKED_BYTES=$(du -bd 0 metadata.json plugins/ resources/ | cut -f 1 | tr "\n" "+" | sed "s/+$/\n/" | bc)
 echo "UNPACKED_BYTES: $UNPACKED_BYTES"
@@ -15,17 +44,14 @@ echo "UNPACKED_BYTES: $UNPACKED_BYTES"
 rm package.zip || true
 zip -r package.zip metadata.json plugins/ resources/ -x "plugins/.git/**" -x plugins/.gitignore -x plugins/README.md -x "plugins/images/**" -x "**/*.fbp"
 
-# Get the current tag, if one exists.
-CURRTAG="$(cd plugins; git describe --tags)"
-
-echo "
-JSON ENTRY:
-{
-    'version': '$CURRTAG',
-    'status': 'stable',
-    'kicad_version': '7.0',
-    'download_sha256': '$(sha256sum package.zip | cut -d ' ' -f 1)',
-    'download_size': $(du -b package.zip | cut -f 1),
-    'download_url': 'https://github.com/YOUR/DOWNLOAD/URL/kicad-beautiful-theme.zip',
-    'install_size': $UNPACKED_BYTES
+# Add or update the JSON entry:
+JSONSTR="{
+    \"version\": \"$CURRTAG\",
+    \"status\": \"stable\",
+    \"kicad_version\": \"7.0\",
+    \"download_sha256\": \"$(sha256sum package.zip | cut -d ' ' -f 1)\",
+    \"download_size\": $(du -b package.zip | cut -f 1),
+    \"download_url\": \"https://github.com/gauravmm/HierarchicalPcb/releases/download/$CURRTAG/package.zip\",
+    \"install_size\": $UNPACKED_BYTES
 }"
+python3 update_metadata.py "$JSONSTR"
---
+++ HierarchicalPCB/update_metadata.py
@@ -0,0 +1,17 @@
+import json
+from pathlib import Path
+import sys
+
+filename = Path("metadata.json")
+# Provide the new version as a json string
+merge = json.loads(sys.argv[1])
+
+metadata = json.loads(filename.read_text())
+for i, v in enumerate(metadata["versions"]):
+    if v["version"] == merge["version"]:
+        metadata["versions"][i] = merge
+        break
+else:
+    metadata["versions"].append(merge)
+
+filename.write_text(json.dumps(metadata, indent=4))
